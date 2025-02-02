44ee4d1f7f1da2badde3785cd9f6806018e9744e
anshu1612 <anshuchakrawarti26@gmail.com>
2025-02-02 17:34:39 +0530

    Displaying platform icons

--- src/components/GameCard.jsx
+++ src/components/GameCard.jsx
@@ -1,15 +1,17 @@
-import { Card, CardBody, Heading, Image } from '@chakra-ui/react'
-import React from 'react'
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+import React from "react";
+import PlatformIconLists from "./PlatformIconLists";
 
-const GameCard = ({game}) => {
+const GameCard = ({ game }) => {
   return (
-    <Card borderRadius={10} overflow='hidden'>
-        <Image src={game.background_image}/>
-        <CardBody>
-            <Heading fontSize='2xl'>{game.name}</Heading>
-        </CardBody>
+    <Card borderRadius={10} overflow="hidden">
+      <Image src={game.background_image} />
+      <CardBody>
+        <Heading fontSize="2xl">{game.name}</Heading>
+        <PlatformIconLists platforms={game.parent_platforms.map(p=>p.platform)}/>
+      </CardBody>
     </Card>
-  )
-}
+  );
+};
 
-export default GameCard
+export default GameCard;
---
+++ src/components/PlatformIconLists.jsx
@@ -0,0 +1,36 @@
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaLinux,
+  FaApple,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+
+const PlatformIconLists = ({ platforms }) => {
+  const iconMap={
+    pc:FaWindows,
+    playstation:FaPlaystation,
+    xbox:FaXbox,
+    nintendo:SiNintendo,
+    mac:FaApple,
+    linux:FaLinux,
+    ios:MdPhoneIphone,
+    web:BsGlobe,
+    android:FaAndroid
+  }
+  
+    return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color='gray.500'/>
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconLists;
